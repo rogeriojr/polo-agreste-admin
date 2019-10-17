b38214edd55a37753b30646bada29adbc525fc05
gabriel-pena <habbosuperboss@gmail.com>
2019-10-17 08:50:26 -0300

    Colocar cores no menu de produtos

--- src/containers/App/menu.js
+++ src/containers/App/menu.js
@@ -14,6 +14,9 @@ import ProductListPage from 'containers/Pages/ProductMenu/ListPage/Loadable';
 import ProductInsertPage from 'containers/Pages/ProductMenu/InsertPage/Loadable';
 import ProductUpdatePage from 'containers/Pages/ProductMenu/UpdatePage';
 import ProductImportPage from 'containers/Pages/ProductMenu/ImportPage/Loadable';
+import ColorListPage from 'containers/Pages/SettingMenu/ColorListPage/Loadable';
+import ColorInsertPage from 'containers/Pages/SettingMenu/ColorInsertPage/Loadable';
+import ColorUpdatePage from 'containers/Pages/SettingMenu/ColorUpdatePage/Loadable';
 
 // Menu Categorias
 import CategoryListPage from 'containers/Pages/CategoryMenu/ListPage/Loadable';
@@ -46,9 +49,6 @@ import ProfileAccessInsertPage from 'containers/Pages/SettingMenu/ProfileAccessI
 import UserListPage from 'containers/Pages/SettingMenu/UserListPage/Loadable';
 import UserNewPage from 'containers/Pages/SettingMenu/UserInsertPage/Loadable';
 import UserUpdatePage from 'containers/Pages/SettingMenu/UserUpdatePage/Loadable';
-import ColorListPage from 'containers/Pages/SettingMenu/ColorListPage/Loadable';
-import ColorInsertPage from 'containers/Pages/SettingMenu/ColorInsertPage/Loadable';
-import ColorUpdatePage from 'containers/Pages/SettingMenu/ColorUpdatePage/Loadable';
 
 // Menu Página
 import PageListPage from 'containers/Pages/PageMenu/ListPage/Loadable';
@@ -116,6 +116,25 @@ const Menu = [
         text: 'Importar Produtos',
         url: '/stock-import',
         component: ProductImportPage,
+      },
+      {
+        id: 'color-list',
+        text: 'Listar Cor',
+        url: '/color',
+        component: ColorListPage,
+      },
+      {
+        id: 'color-new',
+        text: 'Cadastrar Cor',
+        url: '/color/new',
+        component: ColorInsertPage,
+      },
+      {
+        id: 'color-update',
+        text: 'Atualizar Cor',
+        url: '/color/update/:id',
+        component: ColorUpdatePage,
+        showInMenu: false,
       },
     ],
   },
@@ -274,25 +293,6 @@ const Menu = [
         text: 'Atualizar Usuário',
         url: '/user/update/:id',
         component: UserUpdatePage,
-        showInMenu: false,
-      },
-      {
-        id: 'color-list',
-        text: 'Listar Cor',
-        url: '/color',
-        component: ColorListPage,
-      },
-      {
-        id: 'color-new',
-        text: 'Cadastrar Cor',
-        url: '/color/new',
-        component: ColorInsertPage,
-      },
-      {
-        id: 'color-update',
-        text: 'Atualizar Cor',
-        url: '/color/update/:id',
-        component: ColorUpdatePage,
         showInMenu: false,
       },
     ],
