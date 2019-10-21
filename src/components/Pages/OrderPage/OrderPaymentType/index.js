d7e8ae4bf248cabd5537314bf83a2c357f61ee09
gabriel-pena <habbosuperboss@gmail.com>
2019-10-21 16:37:26 -0300

    Legenda de icone

--- src/components/Pages/OrderPage/OrderPaymentType/index.js
+++ src/components/Pages/OrderPage/OrderPaymentType/index.js
@@ -7,9 +7,22 @@ const OrderPaymentType = ({ rowData }) => {
   const paymentType = Number(rowData.payment_type);
   return (
     <>
-      {paymentType === 1 && <Icon>credit_card</Icon>}
+      {paymentType === 1 && (
+        <>
+          <Icon style={{ verticalAlign: 'bottom', paddingTop: 2 }}>
+            credit_card
+          </Icon>{' '}
+          Cartão
+        </>
+      )}
       {paymentType === 2 && (
-        <FaBarcode fontSize={20} style={{ paddingLeft: 3 }} />
+        <>
+          <FaBarcode
+            fontSize={20}
+            style={{ paddingLeft: 3, verticalAlign: 'bottom', paddingTop: 0 }}
+          />{' '}
+          Boleto
+        </>
       )}
     </>
   );
