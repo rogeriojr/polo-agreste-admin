e593d21e55ac5e066e6590454d01245b8cf89010
Welington da Silva <thebylito@Welingtons-Mac-mini.local>
2020-01-28 22:40:15 -0300

    Fix: Comentado propriedades que nao vem da API

--- src/components/Pages/ProductPage/ProductStatus/index.js
+++ src/components/Pages/ProductPage/ProductStatus/index.js
@@ -7,10 +7,11 @@ const ProductStatus = ({ rowData }) => (
     {rowData.status === 0 && <LegendStatus color="#FF5151" letter="I" />}
     {rowData.status === 1 && <LegendStatus color="#00A146" letter="A" />}
     {rowData.status === 2 && <LegendStatus color="#9B6B34" letter="B" />}
-    {Number(rowData.stock_control) === 1 && rowData.stock <= 0 && (
+    {/* {Number(rowData.stock_control) === 1 && rowData.stock <= 0 && (
       <LegendStatus color="#EFA700" letter="E" />
     )}
-    {rowData.images.length === 0 && <LegendStatus color="#B145E6" letter="M" />}
+    TODO: Propriendades nao vem da API
+    {rowData.images.length === 0 && <LegendStatus color="#B145E6" letter="M" />} */}
   </div>
 );
 
